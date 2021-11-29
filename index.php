a19625cab17faf49a8780775552398f798dab3a6
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-29 13:58:54 +0700

    bwa php mysql initialization

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MySQL</title>
+</head>
+<body>
+    <h2>Learn PHP and MySQL</h2>
+    <p>This is my first php and mysql</p>
+
+    <?php
+        // komentar untuk 1 baris
+        /* komentar multi baris
+            # variable: wadah untuk menampung nilai tertentu 
+            # untuk mendeklarasi variable diawali dengan simbol $ 
+        */
+        $myname = "Yusuf Rizal";
+
+        echo("Learn PHP MySQL at Inixindo Surabaya!");
+        echo("<br>");
+        echo($myname);
+    ?>
+</body>
+</html>
