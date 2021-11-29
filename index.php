612c9e50f1097acf2ac3869b28fdd2c8549890dd
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-29 14:20:07 +0700

    bwa php mysql - operator aritmatika

--- index.php
+++ index.php
@@ -16,11 +16,29 @@
             # variable: wadah untuk menampung nilai tertentu 
             # untuk mendeklarasi variable diawali dengan simbol $ 
         */
-        $myname = "Yusuf Rizal";
+        $myname = "Yusuf Rizal";    // tipe data string
+        $myage = 30;                // tipe data integer 
+        $myweight = 60.54;          // tipe data double
+        $angka1 = 12;
+        $angka2 = 20;
+        $angka3 = 5;
+        $penjumlahan = $angka1 + $angka2;
+        $pengurangan = $angka1 - $angka2;
+        $perkalian = $angka1 * $angka2;
+        $pembagian = $angka1 / $angka2;
+        $rumus = ($angka1 + $angka2) - $angka3;
 
         echo("Learn PHP MySQL at Inixindo Surabaya!");
         echo("<br>");
-        echo($myname);
+        echo("My name is " . $myname);
+        echo("<br>");
+        echo("My age is " . $myage);
+        echo("<br>");
+        echo("Angka 1: $angka1 + Angka 2: $angka2 = $penjumlahan");echo("<br>");
+        echo("Angka 1: $angka1 - Angka 2: $angka2 = $pengurangan");echo("<br>");
+        echo("Angka 1: $angka1 * Angka 2: $angka2 = $perkalian");echo("<br>");
+        echo("Angka 1: $angka1 / Angka 2: $angka2 = $pembagian");echo("<br>");
+        echo("Hasil rumus adalah " . $rumus);
     ?>
 </body>
 </html>
