16cd09841c66d1a9250b9bd7ce2529fb8492d015
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-30 10:39:45 +0700

    bwa php mysql - arrays: multidimension

--- arrays.php
+++ arrays.php
@@ -22,13 +22,44 @@
     // indexed array
     $mobil = array("Sedan", "Truck", "Jeep", "SUV", "Sport", "Bus");
     $hitungMobil = count($mobil);
-    echo "Banyaknya mobil ada " . $hitungMobil . " yaitu: <br>";
+    // echo "Banyaknya mobil ada " . $hitungMobil . " yaitu: <br>";
     for ($mobildata = 0; $mobildata < $hitungMobil; $mobildata++) {
-        echo $mobil[$mobildata] . "<br>";
+        // echo $mobil[$mobildata] . "<br>";
     }
 
     // associative array
-    
+    $person = array("Peter" => "20", "Louis" => "24", "Diego" => "40", "Francisco" => "34", "Stephanie" => "28");
+    // echo "List of persons: <br>";
+    foreach ($person as $key => $value) {
+        // echo "Name is " . $key . " and age is " . $value . "<br>";
+    }
+
+    // multidimension array
+    /* data mobil:
+        > merk  BMW     Volvo   Bajaj
+        > tipe  sedan   Sport   Bus
+        > roda  4       4       12
+    */
+    $datamobil = array(
+        array("BMW", "Sedan", 4),
+        array("Volvo", "Sport", 4),
+        array("Bajaj", "Bus", 12),
+        array("Scania", "Bus", 14),
+    );
+    for ($baris = 0; $baris < 4; $baris++) {
+        echo "<p>Baris ke $baris</p>";
+        echo "<ul>";
+        for ($kolom = 0; $kolom < 3; $kolom++) {
+            echo "<li>" . $datamobil[$baris][$kolom] . "</li>";
+        }
+        echo "</ul>";
+    }
+
+    /*
+        Untuk membuat list:
+        > ul (unordered list) -> bullet
+        > ol (ordered list) -> numbering
+    */
 
     ?>
 </body>
