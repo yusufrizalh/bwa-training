c4d4689aa0db276cb23920bce74c130ef40a3e52
Yusuf Rizal <yusufrizalh@gmail.com>
2021-12-01 14:55:28 +0700

    bwa php mysql - bootstrap: table

--- formtable.php
+++ formtable.php
@@ -6,21 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Employee</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 
 <body>
-    <h2>Form Add New Employee</h2>
-    <h3><a href="showtable.php">Show Employees</a></h3>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
-    <form action="formtableaction.php" method="post">
-        <label for="emp_name">Name</label>
-        <input type="text" name="emp_name" placeholder="Enter your name here" required>
-        <br>
-        <label for="emp_email">Email</label>
-        <input type="email" name="emp_email" placeholder="Enter email address here" required>
-        <br>
-        <input type="submit" value="Save">
-    </form>
+    <div class="container">
+        <h2>Form Add New Employee</h2>
+        <h3><a href="showtable.php" class="btn btn-link">Show Employees</a></h3>
+
+        <form action="formtableaction.php" method="post">
+            <div class="form-group">
+                <label for="emp_name">Name</label>
+                <input type="text" class="form-control name=" emp_name" placeholder="Enter your name here" required>
+            </div>
+            <div class="form-group">
+                <label for="emp_email">Email</label>
+                <input type="email" class="form-control name=" emp_email" placeholder="Enter email address here" required>
+            </div>
+            <input type="submit" class="btn btn-outline-info" value="Save">
+        </form>
+    </div>
 </body>
 
 </html>
--- showtable.php
+++ showtable.php
@@ -6,63 +6,91 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Show Employee</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 
 <body>
-    <h2>Show data from employee table</h2>
-    <h3><a href="formtable.php">Add Employee</a></h3>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
-    <table border="1" width="70%">
-        <tr>
-            <th>Employee ID</th>
-            <th>Employee Name</th>
-            <th>Employee Email</th>
-            <th>Employee Hiredate</th>
-        </tr>
-        <?php
-        class TableRows extends RecursiveIteratorIterator
-        {
-            function __construct($iterator)
+    <div class="container">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="./images/image-1.jpg" class="d-block w-100" alt="Image1">
+                </div>
+                <div class="carousel-item">
+                    <img src="./images/image-2.jpg" class="d-block w-100" alt="Image2">
+                </div>
+                <div class="carousel-item">
+                    <img src="./images//image-3.jpg" class="d-block w-100" alt="Image3">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </button>
+        </div>
+        <hr>
+        <h2>Show data from employee table</h2>
+        <h3><a href="formtable.php" class="btn btn-link">Add Employee</a></h3>
+
+        <table class="table table-hover table-striped table-bordered">
+            <tr>
+                <th>Employee ID</th>
+                <th>Employee Name</th>
+                <th>Employee Email</th>
+                <th>Employee Hiredate</th>
+            </tr>
+            <?php
+            class TableRows extends RecursiveIteratorIterator
             {
-                parent::__construct($iterator, self::LEAVES_ONLY);
+                function __construct($iterator)
+                {
+                    parent::__construct($iterator, self::LEAVES_ONLY);
+                }
+                function current()
+                {
+                    return "<td>" . parent::current() . "</td>";
+                }
+                function beginChildren()
+                {
+                    echo "<tr>";
+                }
+                function endChildren()
+                {
+                    echo "</tr>";
+                }
             }
-            function current()
-            {
-                return "<td>" . parent::current() . "</td>";
-            }
-            function beginChildren()
-            {
-                echo "<tr>";
-            }
-            function endChildren()
-            {
-                echo "</tr>";
-            }
-        }
 
-        $host = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "db_bwa";
+            $host = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "db_bwa";
 
-        try {
-            $dbconn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-            $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            try {
+                $dbconn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+                $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $stmt = $dbconn->prepare("SELECT * FROM employee ORDER BY emp_id ASC");
-            // $stmt = $dbconn->prepare("SELECT * FROM employee WHERE emp_name LIKE '%p%'");
-            $stmt->execute();
+                $stmt = $dbconn->prepare("SELECT * FROM employee ORDER BY emp_id ASC");
+                // $stmt = $dbconn->prepare("SELECT * FROM employee WHERE emp_name LIKE '%p%'");
+                $stmt->execute();
 
-            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $key => $value) {
-                echo $value;
+                $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $key => $value) {
+                    echo $value;
+                }
+            } catch (PDOException $ex) {
+                echo "Error: " . $ex->getMessage();
             }
-        } catch (PDOException $ex) {
-            echo "Error: " . $ex->getMessage();
-        }
-        $dbconn = null;
-        ?>
-    </table>
+            $dbconn = null;
+            ?>
+        </table>
+    </div>
 </body>
 
 </html>
